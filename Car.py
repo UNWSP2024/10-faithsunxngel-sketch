e9399cb9295d5bfdbdfd8f01ab831497e463ecca
faithsunxngel-sketch <faithsunxngel@gmail.com>
2025-11-07 16:37:03 -0600

    Week 10 programs
    Week 10 programs

--- Car.py
+++ Car.py
@@ -12,3 +12,37 @@
 # The brake method should subtract 5 from the speed data attribute each time it is called.
 # The get_speed method should return the current speed.
 # Next, design a program that creates a Car object then calls the accelerate method five times.  After each call to the accelerate method, get the current speed of the car and display it.  The call the brake method.  After each call to the brake method, get the current speed of the car and display it.
+
+
+class Car:
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        self.__speed -= 5
+
+    def get_speed(self):
+        return self.__speed
+
+
+def main():
+    my_car = Car(2024, "Toyota")
+
+    print("Accelerating...")
+    for i in range(5):
+        my_car.accelerate()
+        print(f"Current speed after accelerating {i + 1} time(s): {my_car.get_speed()} mph")
+
+    print("\nBraking...")
+    for i in range(5):
+        my_car.brake()
+        print(f"Current speed after braking {i + 1} time(s): {my_car.get_speed()} mph")
+
+
+if __name__ == '__main__':
+    main()
